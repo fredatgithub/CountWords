06d70aca2711e98ac1eed66b3f7f324e867e7abd
fred <domaine75@gmail.com>
2022-01-06 09:22:46 +0100

    Ajoutez des fichiers projet.

---
+++ CountWords/FormMain.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CountWords
+{
+  partial class FormMain
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.textBoxSource = new System.Windows.Forms.TextBox();
+      this.buttonExtract = new System.Windows.Forms.Button();
+      this.buttonSaveFile = new System.Windows.Forms.Button();
+      this.progressBarMain = new System.Windows.Forms.ProgressBar();
+      this.listBoxDico = new System.Windows.Forms.ListBox();
+      this.buttonRemove = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // textBoxSource
+      // 
+      this.textBoxSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+      this.textBoxSource.Location = new System.Drawing.Point(12, 67);
+      this.textBoxSource.Multiline = true;
+      this.textBoxSource.Name = "textBoxSource";
+      this.textBoxSource.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+      this.textBoxSource.Size = new System.Drawing.Size(209, 334);
+      this.textBoxSource.TabIndex = 0;
+      // 
+      // buttonExtract
+      // 
+      this.buttonExtract.Location = new System.Drawing.Point(233, 108);
+      this.buttonExtract.Name = "buttonExtract";
+      this.buttonExtract.Size = new System.Drawing.Size(75, 23);
+      this.buttonExtract.TabIndex = 2;
+      this.buttonExtract.Text = "Extract";
+      this.buttonExtract.UseVisualStyleBackColor = true;
+      this.buttonExtract.Click += new System.EventHandler(this.ButtonExtract_Click);
+      // 
+      // buttonSaveFile
+      // 
+      this.buttonSaveFile.Location = new System.Drawing.Point(233, 337);
+      this.buttonSaveFile.Name = "buttonSaveFile";
+      this.buttonSaveFile.Size = new System.Drawing.Size(75, 23);
+      this.buttonSaveFile.TabIndex = 3;
+      this.buttonSaveFile.Text = "Save";
+      this.buttonSaveFile.UseVisualStyleBackColor = true;
+      this.buttonSaveFile.Click += new System.EventHandler(this.ButtonSaveFile_Click);
+      // 
+      // progressBarMain
+      // 
+      this.progressBarMain.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.progressBarMain.Location = new System.Drawing.Point(0, 427);
+      this.progressBarMain.Name = "progressBarMain";
+      this.progressBarMain.Size = new System.Drawing.Size(588, 23);
+      this.progressBarMain.TabIndex = 4;
+      // 
+      // listBoxDico
+      // 
+      this.listBoxDico.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+      this.listBoxDico.FormattingEnabled = true;
+      this.listBoxDico.Location = new System.Drawing.Point(321, 67);
+      this.listBoxDico.Name = "listBoxDico";
+      this.listBoxDico.Size = new System.Drawing.Size(231, 329);
+      this.listBoxDico.Sorted = true;
+      this.listBoxDico.TabIndex = 5;
+      // 
+      // buttonRemove
+      // 
+      this.buttonRemove.Location = new System.Drawing.Point(233, 137);
+      this.buttonRemove.Name = "buttonRemove";
+      this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+      this.buttonRemove.TabIndex = 6;
+      this.buttonRemove.Text = "Remove ->";
+      this.buttonRemove.UseVisualStyleBackColor = true;
+      this.buttonRemove.Click += new System.EventHandler(this.ButtonRemove_Click);
+      // 
+      // FormMain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(588, 450);
+      this.Controls.Add(this.buttonRemove);
+      this.Controls.Add(this.listBoxDico);
+      this.Controls.Add(this.progressBarMain);
+      this.Controls.Add(this.buttonSaveFile);
+      this.Controls.Add(this.buttonExtract);
+      this.Controls.Add(this.textBoxSource);
+      this.Name = "FormMain";
+      this.Text = "Create dictionary";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox textBoxSource;
+    private System.Windows.Forms.Button buttonExtract;
+    private System.Windows.Forms.Button buttonSaveFile;
+    private System.Windows.Forms.ProgressBar progressBarMain;
+    private System.Windows.Forms.ListBox listBoxDico;
+    private System.Windows.Forms.Button buttonRemove;
+  }
+}
+
---
+++ CountWords/FormMain.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CountWords
+{
+  public partial class FormMain : Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private void ButtonExtract_Click(object sender, EventArgs e)
+    {
+      if (string.IsNullOrEmpty(textBoxSource.Text))
+      {
+        MessageBox.Show("La source ne peut pas être vide");
+        return;
+      }
+
+      var listOfWords = new List<string>();
+      string source = textBoxSource.Text;
+      var tableau = source.Split(' ');
+      progressBarMain.Minimum = 0;
+      progressBarMain.Maximum = tableau.Length;
+      progressBarMain.Value = progressBarMain.Minimum;
+      int i = 0;
+      foreach (var word in tableau)
+      {
+        string word2 = RemoveBadCharacters(word);
+        if (!listOfWords.Contains(word2.ToLower()) && word2 != string.Empty)
+        {
+          listOfWords.Add(word2.ToLower());
+        }
+
+        progressBarMain.Value = i++;
+      }
+
+      progressBarMain.Value = progressBarMain.Minimum;
+      progressBarMain.Maximum = listOfWords.Count;
+      i = 0;
+      foreach (var word in listOfWords)
+      {
+        if (!listBoxDico.Items.Contains(word.ToLower()))
+        {
+          listBoxDico.Items.Add(word.ToLower());
+        }
+        
+        progressBarMain.Value = i++;
+      }
+
+      progressBarMain.Value = progressBarMain.Minimum;
+    }
+
+    private string RemoveBadCharacters(string word)
+    {
+      string result = string.Empty;
+      result = word.Trim('.');
+      result = result.Trim(',');
+      result = result.Trim('"');
+      result = result.TrimStart('"');
+      result = result.Trim('!');
+      result = result.Trim('?');
+      result = result.Trim('=');
+      result = result.TrimEnd(';');
+      if (result == " " || result.Length == 1)
+      {
+        result = string.Empty;
+      }
+
+      if (result.StartsWith("\""))
+      {
+        result = result.Substring(1);
+      }
+
+      return result;
+    }
+
+    private void ButtonSaveFile_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        using (StreamWriter sw = new StreamWriter("dico.txt"))
+        {
+          foreach (string word in listBoxDico.Items)
+          {
+            sw.WriteLine(word);
+          }
+        }
+
+        MessageBox.Show("Un fichier dico.txt a été enregistré.");
+      }
+      catch (Exception exception)
+      {
+        MessageBox.Show(exception.Message);
+      }
+    }
+
+    private void ButtonRemove_Click(object sender, EventArgs e)
+    {
+      if (listBoxDico.SelectedIndex != -1)
+      {
+        listBoxDico.Items.RemoveAt(listBoxDico.SelectedIndex);
+      }
+      else
+      {
+        MessageBox.Show("Vous devez sélectionner un caractère");
+      }
+    }
+  }
+}
