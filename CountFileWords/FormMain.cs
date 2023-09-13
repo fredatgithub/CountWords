a3528beda99aacbc4b79161dfce4db106a0da827
Freddy JUHEL <ext.freddy.juhel@reseau.sncf.fr>
2023-09-13 15:52:58 +0200

    Add another project

---
+++ CountFileWords/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountFileWords
+{
+  public partial class FormMain : Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private void QuitterToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+
+    private void FormMain_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
